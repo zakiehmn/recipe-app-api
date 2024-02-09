9b11b32bb1a73ff6ed400b50e5fc754099df3cb9
zakiehmn <zakiehmantashloo@gmail.com>
2024-02-09 13:43:51 +0330

    configured docker compose and postgres db

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator functions 
+"""
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x + y
+
+def subtract(x,y):
+    """subtract x from y and return result"""
+    return y - x
+    
